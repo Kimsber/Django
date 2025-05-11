db624b5581b1b3931f3d666cd9c6b4accb986502
Kim.H <kim60309@gmail.com>
2025-05-11 22:34:16 +0800

    新增ru6資料模型todo

---
+++ todo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(
+        null=True, blank=True
+    )  # null為物件，blank為字串
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id} - {self.title} - {self.created}"
