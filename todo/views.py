2cb5d2aa50d892560b24a46a7125d5a97af39008
Kim.H <kim60309@gmail.com>
2025-05-12 21:52:20 +0800

    完成首頁-代辦事項

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
+
 
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    result = {
+        "todos": todos,
+    }
+    return render(request, "todo/todo.html", result)
